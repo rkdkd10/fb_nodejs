d793676728dcc11db4f0c43a50ae0c8952ea59ea
rkdkd10 <121798218+rkdkd10@users.noreply.github.com>
2025-05-06 11:59:29 +0530

    updated

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://fb-react-omega.vercel.app/api/data'],
+  credentials: true
+}));
 app.use(express.json());
 const upload = multer();
 
